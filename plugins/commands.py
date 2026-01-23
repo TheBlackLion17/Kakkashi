6a2396f6916c167d49b87aca4dba5f71937f5e41
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2026-01-24 00:37:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,42 +1,40 @@
-# plugins/commands.py
-
 import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from info import PICS
-from Script import *
+from Script import script   # ✅ IMPORT CLASS
+
+
+print("✅ commands.py loaded")
 
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_cmd(client, message):
 
+    user = message.from_user
     pic = random.choice(PICS) if PICS else None
+
+    text = script.START_TXT.format(
+        user.first_name,
+        user.mention
+    )
 
     buttons = InlineKeyboardMarkup(
         [
-            [
-                InlineKeyboardButton("🎥 Movies", callback_data="movies"),
-                InlineKeyboardButton("ℹ️ Help", callback_data="help")
-            ],
-            [
-                InlineKeyboardButton("📢 Updates", url="https://t.me/YourChannel")
-            ]
+            [InlineKeyboardButton("ℹ️ Help", callback_data="help")],
+            [InlineKeyboardButton("➕ Add Me", url=f"https://t.me/{client.me.username}?startgroup=true")]
         ]
     )
 
     if pic:
         await message.reply_photo(
             photo=pic,
-            caption=START_TEXT.format(
-                mention=message.from_user.mention
-            ),
+            caption=text,
             reply_markup=buttons
         )
     else:
         await message.reply_text(
-            text=START_TEXT.format(
-                mention=message.from_user.mention
-            ),
+            text=text,
             reply_markup=buttons
         )
