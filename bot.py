99aab0f240c4f4ded9cfe2cc94c4bc146b64d183
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2026-01-23 21:12:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ async def Lucy_start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("Lucy Bot Imported => " + plugin_name)
+            print("Ags Bot Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server()) 
     b_users, b_chats = await db.get_banned()
@@ -101,3 +101,4 @@ if __name__ == '__main__':
         loop.run_until_complete(Lucy_start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
+
